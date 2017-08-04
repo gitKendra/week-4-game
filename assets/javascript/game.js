34ca700da87726789499903915f2d12895b822d1
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-08-04 08:37:21 -0400

    switch pc

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+
+// Create the game character objects
+
+var player1 = {
+	name = "p1",
+	hp = 0,
+	ap = 0,
+	numAttack = 0
+};
+
+var player2 = {
+	name = "p1",
+	hp = 0,
+	ap = 0,
+	numAttack = 0
+};
+
+var player3 = {
+	name = "p1",
+	hp = 0,
+	ap = 0,
+	numAttack = 0
+};
+
+var player4 = {
+	name = "p1",
+	hp = 0,
+	ap = 0,
+	numAttack = 0
+};
+
+
+// Create variables to store calls to HTML elements
+var attackField;
+var defendField;
+var playerField;
+var infoField;
